caec39938665a38284a88829ee604c09ad680bc2
fzramos <64625115+fzramos@users.noreply.github.com>
2020-12-02 18:40:07 -0600

    API search for books and genre is getting info -FR

--- book_recs_app/static/js/main.js
+++ book_recs_app/static/js/main.js
@@ -1,24 +1,37 @@
-
+// globals
 
 
 // Search page functions
-const getBooks = () => {
+const bookInfoHelper = async () => {
     let search_text = document.getElementById('search-input').value;
     alert(`The form was submitted with the value: ${search_text}`);
 
-    fetchResults(search_text)
+    // will get this 
+    const rawResults = await getBooksInfo(search_text)
+    if (rawResults.totalItems == 0) {
+        // TODO make this show message to user saying try again
+        console.log('No results')
+    } else {
+        rawResults.items.forEach(i => {
+            console.log(i.volumeInfo.title)
+        })
+    }
+    
+    console.log(rawResults)
 
+    
 
 }
 
-async function fetchResult() {
-    let response = await fetch('/readme.txt');
-    let data = await response.json();
-    console.log(data);
+async function getBooksInfo(book) {
+    // TODO set max to more
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${book}&maxResults=1`);
+    const data = await response.json();
+    return data;
 }
 
 const searchGenre = () => {
-    document.getElementById('search-input').value = 'subject: ';
+    document.getElementById('search-input').value = 'subject:' + document.getElementById('search-input').value;
     document.getElementById('search-input').focus();
 
 }
